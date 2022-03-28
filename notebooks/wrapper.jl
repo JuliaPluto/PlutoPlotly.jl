261d915c29d7cd66007b82b9997a427745f253d9
Alberto Mengali <disberd@gmail.com>
2022-03-28 16:18:53 +0200

    better docs for mathjax hack

--- notebooks/wrapper.jl
+++ notebooks/wrapper.jl
@@ -222,12 +222,14 @@ const FORCE_MATHJAX_LOCAL = Ref(false)
 
 # ╔═╡ 49cf85b1-bf09-49de-8468-4e240dc621fa
 """
-	pluto_mathjax_local::Bool
-	pluto_mathjax_local(flag::Bool)::Bool
+	force_pluto_mathjax_local::Bool
+	force_pluto_mathjax_local(flag::Bool)::Bool
 
-Eventually set the flag to force mathjax svg output in pluto to be locally cached and return its current value.
+Returns `true` if the `PlutoPlot` `show` method forces svgs produced by MathJax to be locally cached and `false` otherwise.
 
-This is used to make mathjax in recent plolty versions (>2.10) work as expected. The default `global` caching in Pluto creates problems with the math display.
+The flag can be set at package level by providing the intended boolean value as argument to the function
+
+Local svg caching is used to make mathjax in recent plolty versions (>2.10) work as expected. The default `global` caching in Pluto creates problems with the math display.
 """
 force_pluto_mathjax_local() = FORCE_MATHJAX_LOCAL[]
 
