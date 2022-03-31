b33433fda0e415e466b8f4d3320cb0468145b176
Alberto Mengali <disberd@gmail.com>
2022-03-31 14:37:05 +0200

    Update main package file

--- src/PlutoPlotly.jl
+++ src/PlutoPlotly.jl
@@ -3,7 +3,13 @@ module PlutoPlotly
 using Reexport
 @reexport using PlotlyBase
 
-export PlutoPlot, get_plotly_version, change_plotly_version, check_plotly_version, force_pluto_mathjax_local, htl_js
+export PlutoPlot, get_plotly_version, change_plotly_version, check_plotly_version, force_pluto_mathjax_local, htl_js, add_plotly_listener!
 include("../notebooks/wrapper.jl")
+
+function __init__()
+	if !is_inside_pluto()
+		@warn "You loaded this package outside of Pluto, this is not the intended behavior and you should use either PlotlyBase or PlotlyJS directly"
+	end
+end
 
 end
