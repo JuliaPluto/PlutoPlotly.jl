80189b7326e49ace59fbd51263dd1824ab0f8e2e
Alberto Mengali <disberd@gmail.com>
2023-08-27 18:49:08 +0200

    add tests

---
+++ test/extensions.jl
@@ -0,0 +1,25 @@
+using PlutoPlotly
+using Test
+
+## PlotlyKaleido Extension ##
+using PlotlyKaleido
+
+PlotlyKaleido.start()
+
+mktempdir() do dir
+    cd() do 
+        p = plot(rand(10,4))
+        @test_nowarn savefig(p, "test_savefig.png")
+        @test isfile("test_savefig.png")
+    end
+end
+
+## Unitful Extension ##
+using PlutoPlotly: _preprocess
+using Unitful: °, ustrip
+
+uv_r = range(0°, 100°; step = 1°)
+@test _preprocess(uv_r) == collect(0:100)
+uv_a = rand(3,5) .* °
+uv_a_strip = ustrip.(uv_a)
+@test _preprocess(uv_a) == _preprocess(uv_a_strip)
