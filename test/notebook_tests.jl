2fee61d6dc58bb8a576b66187d6f02ddb8c485d4
Alberto Mengali <disberd@gmail.com>
2023-08-28 13:24:20 +0200

    add testing under Pluto

---
+++ test/notebook_tests.jl
@@ -0,0 +1,26 @@
+using Test
+import Pluto: update_save_run!, update_run!, WorkspaceManager, ClientSession,
+ServerSession, Notebook, Cell, project_relative_path, SessionActions,
+load_notebook, Configuration
+
+function noerror(cell; verbose=true)
+    if cell.errored && verbose
+        @show cell.output.body
+    end
+    !cell.errored
+end
+
+
+options = Configuration.from_flat_kwargs(; disable_writing_notebook_files=true)
+notebook_dir = joinpath(@__DIR__, "../notebooks/")
+eval_in_nb(sn, expr) = WorkspaceManager.eval_fetch_in_workspace(sn, expr)
+
+@testset "basic_tests.jl" begin
+    ss = ServerSession(; options)
+    path = joinpath(notebook_dir, "basic_tests.jl")
+    nb = SessionActions.open(ss, path; run_async=false)
+    for cell in nb.cells
+        @test noerror(cell)
+    end
+    SessionActions.shutdown(ss, nb)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
 using SafeTestsets
 
 @safetestset "Extensions" begin include("extensions.jl") end
+@safetestset "Pluto Tests" begin include("notebook_tests.jl") end
