bf2074f52407abbaeafd86a67c09d9eecec43eab
Alberto Mengali <disberd@gmail.com>
2024-02-25 18:30:26 +0100

    disable Malt to get coverage

--- test/notebook_tests.jl
+++ test/notebook_tests.jl
@@ -11,7 +11,7 @@ function noerror(cell; verbose=true)
 end
 
 
-options = Configuration.from_flat_kwargs(; disable_writing_notebook_files=true)
+options = Configuration.from_flat_kwargs(; disable_writing_notebook_files=true, workspace_use_distributed_stdlib = true)
 notebook_dir = joinpath(@__DIR__, "../notebooks/")
 eval_in_nb(sn, expr) = WorkspaceManager.eval_fetch_in_workspace(sn, expr)
 
