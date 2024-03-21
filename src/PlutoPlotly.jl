655ad0c921539cb8a99ebbc7e3a4d6d8a868a986
Alberto Mengali <disberd@gmail.com>
2024-03-21 14:49:56 +0100

    add Aqua

--- src/PlutoPlotly.jl
+++ src/PlutoPlotly.jl
@@ -1,7 +1,6 @@
 module PlutoPlotly
 
-using Reexport
-@reexport using PlotlyBase
+using PlotlyBase
 
 using HypertextLiteral
 using AbstractPlutoDingetjes
@@ -13,7 +12,19 @@ using LaTeXStrings
 using Markdown
 using Downloads: download
 using Artifacts
+# This is similar to `@reexport` but does not exports undefined names and can
+# also avoid exporting the module name
+function re_export(m::Module; skip_modname = false)
+    mod_name = nameof(m)
+    nms = names(m)
+    exprts = filter(nms) do n
+        isdefined(m, n) && (!skip_modname || n != mod_name)
+    end
+    eval(:(using .$mod_name))
+    eval(:(export $(exprts...)))
+end
 
+re_export(PlotlyBase; skip_modname = false)
 export PlutoPlot, get_plotly_version, change_plotly_version,
 check_plotly_version, force_pluto_mathjax_local, htl_js, add_plotly_listener!,
 add_class!, remove_class!, add_js_listener!, default_plotly_template,
@@ -21,6 +32,7 @@ get_image_options, change_image_options!, plutoplotly_paste_receiver
 export plot, push_script!, prepend_cell_selector
 export make_subplots
 export enable_plutoplotly_offline
+
 
 include("local_plotly_library.jl")
 
