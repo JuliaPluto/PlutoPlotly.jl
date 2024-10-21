c5b25ae2c66c82e179d52c2193709ebea99cea55
Alberto Mengali <disberd@gmail.com>
2024-10-21 15:29:17 +0200

    add non-pluto tests of utilities

---
+++ test/utilities.jl
@@ -0,0 +1,16 @@
+@testitem "colorscale_utilities" begin
+    using PlutoPlotly.Colors
+    cs = sample_colorscheme(:viridis)
+    @test length(cs) === 256
+    @test cs.colors isa Vector{<:RGBA}
+    @test all(c -> c.alpha == 1.0, cs.colors)
+
+    css = sample_colorscheme(cs, 10; alpha = 0.5)
+    @test length(css) === 10
+    @test all(c -> c.alpha == 0.5, css.colors)
+
+    # discrete_colorscale
+    dcs = discrete_colorscale(cs, 10)
+    @test length(dcs) === 10 * 2
+    @test dcs isa Vector{Tuple{Float64, String}}
+end
