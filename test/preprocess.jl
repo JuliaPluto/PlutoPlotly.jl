f223ab495a7cc733a02681576e51b48f6594120f
Alberto Mengali <disberd@gmail.com>
2025-07-17 10:32:51 +0200

    add test

--- test/preprocess.jl
+++ test/preprocess.jl
@@ -9,4 +9,18 @@
     tit = d[:title]
     @test haskey(tit, :text) && tit[:text] == "asd"
     @test haskey(tit, :x) && tit[:x] == 0.5
+
+    # Add a test for nested attrs (Cause of issue #65)
+    l = Layout(;
+        sliders = [attr(;
+            steps = [
+                attr(;
+                    label = "Step 1",
+                    value = 1
+                )
+            ]
+        )]
+    )
+    d = _process_with_names(l)
+    @test d[:sliders][1][:steps][1] isa Dict{Symbol}
 end
